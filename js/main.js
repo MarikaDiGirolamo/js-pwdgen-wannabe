db3fc42143920994d007d8be7f27b19d523c492c
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-04-05 16:28:14 +0200

    Completed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+// Inserisci Nome
 
 let nome = prompt ("inserisci nome");
 // console.log (`il nome utente è ${nome}`);
 
+// Inserisci Cognome
 let cognome = prompt ("inserisci il tuo cognome");
 // console.log (`il cognome utente è ${cognome}`);
 
+// Inserisci colore
 let colore = prompt ("inserisci colore");
 // console.log (`il colore utente è ${colore}`);
 
+// Genera Nuova Password
 let password = `${nome} ${cognome} ${colore}`;
 
 document.getElementById("passwordDaInserire").innerHTML = password + " " + 2023;
